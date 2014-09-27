f451017b2bc84ec491c0f65077c758a84e0cbedb
Hatem <hatembenyacoub@Hatems.local>
2014-09-28 00:26:21 +0300

    finalised and tested code added how to use

--- tnelec2014/parser-tsv2json.php
+++ tnelec2014/parser-tsv2json.php
@@ -1,14 +1,20 @@
 <?php
 
 /**
-* Script permettant de convertir données des parrains du txt vers json
+* Script permettant de convertir données des parrains du txt vers json/csv
+* Utilisez pdftotext pour la conversion, ensuite manuellement supprimer 
+* les titres et les entête et garder uniquement le contenu des tableaux
+* Le reste il y'a 5 paramètres à introduire manuellement pour chaque fichier et voilà.
+*
+*  NB : gardez les lignes vides, au cas où un nom de famille, ou du père est inexistant.
+*
 * @since September 27, 2014
 * @author hatem <hatem@php.net>
 * @todo 
 */
 
 //Start edit here
-$file = "tsv/hechmi-elhamdi-txt.tsv";
+$file = "tsv/candidat-txt.tsv";
 
 // items per pages;
 $firstpage = 35;
@@ -34,20 +40,19 @@ foreach($data as $k=>$v){
 echo "First page ($i)\n\n";
 		if ($i < $firstpage){
 			$name[$i]['fname'] = $v;
-		} elseif ($i > $firstpage and $i < $firstpage*2) {
-			$name[($i-$firstpage)]['sname'] = $v;
-		} elseif($i > $firstpage*2 and $i < $firstpage*3) {
-			$name[($i-$firstpage*2)]['familyname'] = $v;
-		} elseif ($i > $firstpage*3 and $i < $firstpage*4) {
+		} elseif ($i >= $firstpage and $i < $firstpage*2) {
+			$name[$i-$firstpage]['sname'] = $v;
+		} elseif($i >= $firstpage*2 and $i < $firstpage*3) {
+			$name[$i-$firstpage*2]['familyname'] = $v;
+		} elseif ($i >= $firstpage*3 and $i < $firstpage*4) {
 			if (!is_numeric($v*1)){die('result is not a CIN :'.$v);}
-			$name[($i-$firstpage*3)]['cin'] = $v;
-				print_r($name); 
-
+			$name[$i-$firstpage*3]['cin'] = $v;
 		}
 
 
 		if ($i == $firstpage *4-1) {
-			$page++;
+			//print_r($name); 
+			$page++;$j =-1;
 		}
 
 	} elseif ( $page == $numpages) { // last page
@@ -55,45 +60,79 @@ echo "Last page\n\n";
 
 
 		if ($j < $lastpage){
+
 			$name[$i]['fname'] = $v;
-		} elseif ($j > $lastpage and $j < $lastpage*2) {
-			$name[$i-$lastpage]['sname'] = $v;
-		} elseif($j > $lastpage*2 and $j < $lastpage*3) {
-			$name[$i-$lastpage*2]['familyname'] = $v;
-		} elseif ($j > $lastpage*3 and $j < $lastpage*4) {
-			if (!is_numeric($v)){die('result is not a CIN :'.$v);}
-			$name[$i-$lastpage*3]['cin'] = $v;
-				print_r($name[$i]); 
+
+		} elseif ($j >= $lastpage and $j < $lastpage*2) {
+
+			$name[$i-$otherpages]['sname'] = $v;
+
+		} elseif($j >= $lastpage*2 and $j < $lastpage*3) {
+
+			$name[$i-$otherpages*2]['familyname'] = $v;
+
+		} elseif ($j >= $lastpage*3 and $j < $lastpage*4) {
+
+			if (!is_numeric($v*1)){die('result is not a CIN :'.$v);}
+
+			$name[$i-$otherpages*3]['cin'] = $v;
 
 		}
 
 
 		if ($i == $lastpage *4) {
+			//print_r($name); 
 			$page++;
 		}
 
 	} else {  // other page
 
-echo "Page number $page ($i)\n\n";
-
+echo "Page number $page\n\n";
 
 		if ($j < $otherpages){
+
 			$name[$i]['fname'] = $v;
-		} elseif ($j > $otherpages and $j < $otherpages*2) {
+
+		} elseif ($j >= $otherpages and $j < $otherpages*2) {
+
 			$name[$i-$otherpages]['sname'] = $v;
-		} elseif($j > $otherpages*2 and $j < $otherpages*3) {
+
+		} elseif($j >= $otherpages*2 and $j < $otherpages*3) {
+
 			$name[$i-$otherpages*2]['familyname'] = $v;
-		} elseif ($j > $otherpages*3 and $j < $otherpages*4) {
-			if (!is_numeric($v)){die('result is not a CIN :'.$v);}
+
+		} elseif ($j >= $otherpages*3 and $j < $otherpages*4) {
+
+
+			if (!is_numeric($v*1)){die('result is not a CIN :'.$v);}
+
 			$name[$i-$otherpages*3]['cin'] = $v;
-				print_r($name); 
 		}
 
 
-		if ($i == $otherpages *4) {
-			$page++; $j =0;
+		if ($j == $otherpages *4-1) {
+			$page++; $j =-1;
 		}
 
 	}
 	$i++; $j++;
 }
+
+
+echo "size of name = ".sizeof($name)."\n page number : $page / $numpages\n";
+
+$res = json_encode($name);
+
+// save json
+$fp = fopen($file.'.json', 'w');
+fwrite($fp, $res);
+fclose($fp);
+	
+// save csv
+$fp = fopen($file.'.csv', 'w');
+foreach ($name as $fields) {
+    fputcsv($fp, $fields);
+}
+fclose($fp);
+
+
